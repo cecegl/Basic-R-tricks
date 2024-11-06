c1cc51daede502f7e042d83ee5c9b8f215e4e069
Celia González López <cece.glop@gmail.com>
2024-11-06 12:08:24 +0100

    Update sorting files from multiple folders.R

--- sorting files from multiple folders.R
+++ sorting files from multiple folders.R
@@ -1,11 +1,14 @@
+#This code will help you copy files into a folder when you haev multiple origin directories
+
 #This will clean your previous elements from the workspace
 rm(list = ls())
 
-#This will create a list of directory paths to all the subfolders in your main folder
+#This will create a list of directory paths to all the subfolders in your main directory
 dir_path <- list.dirs(path = "path to main repository (with multiple folders)",
                       recursive = FALSE)
 
-#This will search all your subfolders one by one for files of the type you select and then copy each of them to the desired destination
+#This will search all your subfolders one by one for files of the type you select and then copy each of them to the
+#desired destination
 for (i in 1:length(dir_path)) {
   
   files_path <- list.files(path = dir_path[[i]], pattern = "type of file you want to move (.csv, .txt, .tif, etc)",
